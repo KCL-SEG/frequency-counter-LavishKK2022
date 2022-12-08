bfc9951af685ea63e6fd1cededd64f1b090bd8ee
Lavish Kamal Kumar <klavish382@gmail.com>
2022-12-08 17:17:22 +0000

    complete frequency-counter excercise

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,21 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+def count(items, searchTerm):
+    iterator = 0
+
+    for item in items:
+        if item == searchTerm:
+            iterator = iterator + 1
+    
+    return iterator
+        
 
 def frequencies(items):
+
+    strlist = [str(item) for item in items]
+    keyset = set(strlist)
+
     frequencies = {}
-    # Your code goes here
+
+    for key in keyset:
+        frequencies[key] = count(strlist, key)
+
     return frequencies
